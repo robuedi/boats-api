c58c316dea1958e5abb5512a7b2524e2d03a4636
Eduard Robu <robu.edi1@gmail.com>
2023-10-16 09:02:35 +0100

    payments

---
+++ app/Providers/PaymentsProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class PaymentsProvider extends ServiceProvider
+{
+
+}
